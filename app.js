206a69d627d2b37f14d746b2293fe652eafd5cd9
Daniella Maleski <daniella.maleski@gmail.com>
2020-02-21 14:10:25 -0300

    FC

---
+++ app.js
@@ -0,0 +1,8 @@
+'user strict';
+
+console.log('here\'s a hidden message')
+
+let today = new Date();
+let formatDate = today.toDateString();
+let selectElement = document.getElementById('date');
+selectElement.innerHTML = formatDate;
